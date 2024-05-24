7109126d603112b8620097a44f9646a5053d4397
Sebastian Holzapfel <me@sebholzapfel.com>
2024-05-24 22:46:57 +0200

    things happen

--- gateware/sim.cpp
+++ gateware/sim.cpp
@@ -12,8 +12,27 @@ int main(int argc, char** argv) {
     top->trace(tfp, 99);  // Trace 99 levels of hierarchy (or see below)
     tfp->open("simx.fst");
     uint32_t sim_time = 1000000;
+
+    top->psram_idle = 1;
+    top->psram_write_ready = 1;
+    top->psram_read_ready = 1;
+
+    contextp->timeInc(1);
+    top->rst_sync = 1;
+    top->rst_hdmi = 1;
+    top->eval();
+    tfp->dump(contextp->time());
+
+    contextp->timeInc(1);
+    top->rst_sync = 0;
+    top->rst_hdmi = 0;
+    top->eval();
+    tfp->dump(contextp->time());
+
     while (contextp->time() < sim_time && !contextp->gotFinish()) {
         contextp->timeInc(1);
+        top->clk_sync = !top->clk_sync;
+        top->clk_hdmi = !top->clk_hdmi;
         top->eval();
         tfp->dump(contextp->time());
     }
