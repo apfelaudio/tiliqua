ff04abc4268d7d5d050f689dc6ad01fec9d238d7
Sebastian Holzapfel <me@sebholzapfel.com>
2024-06-16 10:28:51 +0200

    basic midi polysynth works, untuned

--- gateware/src/example_dsp/top.py
+++ gateware/src/example_dsp/top.py
@@ -432,6 +432,48 @@ class QuadNCO(wiring.Component):
 
         return m
 
+class MidiPolyTop(wiring.Component):
+
+    i: In(stream.Signature(data.ArrayLayout(ASQ, 4)))
+    o: Out(stream.Signature(data.ArrayLayout(ASQ, 4)))
+
+    i_midi: In(stream.Signature(midi.MidiMessage))
+
+    def elaborate(self, platform):
+        m = Module()
+
+        max_voices = 4
+        m.submodules.voice_tracker = voice_tracker = midi.MidiVoiceTracker(max_voices=max_voices)
+        ncos = [dsp.SawNCO(shift=4) for _ in range(max_voices)]
+        vcas = [dsp.VCA() for _ in range(max_voices)]
+        m.submodules += ncos
+        m.submodules += vcas
+        m.submodules.merge4 = merge4 = dsp.Merge(n_channels=4)
+
+        # MIDI -> voice tracker
+        wiring.connect(m, wiring.flipped(self.i_midi), voice_tracker.i)
+
+        for n in range(max_voices):
+
+            m.d.comb += [
+                # voice.note -> NCO.i
+                ncos[n].i.valid.eq(self.i.valid),
+                ncos[n].i.payload.freq_inc.eq(voice_tracker.o[n].payload.note),
+
+                # voice.vel and NCO.o -> VCA.i
+                vcas[n].i.valid.eq(ncos[n].o.valid),
+                vcas[n].i.payload[0].eq(ncos[n].o.payload),
+                vcas[n].i.payload[1].eq(voice_tracker.o[n].payload.velocity << 8),
+                ncos[n].o.ready.eq(vcas[n].i.ready),
+            ]
+
+            # vcas -> merge4
+            wiring.connect(m, vcas[n].o, merge4.i[n]),
+
+        wiring.connect(m, merge4.o, wiring.flipped(self.o)),
+
+        return m
+
 class MidiCVTop(wiring.Component):
 
     """
@@ -576,6 +618,7 @@ def get_core(name):
         "waveshaper": (False, DualWaveshaper),
         "nco":        (False, QuadNCO),
         "midicv":     (False, MidiCVTop),
+        "midipoly":   (False, MidiPolyTop),
     }
 
     if name not in cores:
