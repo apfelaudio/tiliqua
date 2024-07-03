2d096edf3fe9b61ed3ac15e050c863fb567edb84
Seb Holzapfel <me@sebholzapfel.com>
2024-07-03 11:48:30 +0200

    example_soc/selftest: improve coverage, add CSRs for audio half (#20)

--- gateware/src/tiliqua/eurorack_pmod.py
+++ gateware/src/tiliqua/eurorack_pmod.py
@@ -7,11 +7,13 @@
 
 import os
 
-from amaranth              import *
-from amaranth.build        import *
-from amaranth.lib          import wiring, data
-from amaranth.lib.wiring   import In, Out
-from amaranth.lib.fifo     import AsyncFIFO
+from amaranth                   import *
+from amaranth.build             import *
+from amaranth.lib               import wiring, data
+from amaranth.lib.wiring        import In, Out
+from amaranth.lib.fifo          import AsyncFIFO
+from amaranth.lib.cdc           import FFSynchronizer
+from luna_soc.gateware.csr.base import Peripheral
 
 from amaranth_future       import fixed, stream
 
@@ -27,6 +29,7 @@ class AudioStream(wiring.Component):
     """
     Domain crossing logic to move samples from `eurorack-pmod` logic in the audio domain
     to logic in a different (faster) domain using a stream interface.
+    This is used by most DSP examples for glitch-free audio streaming.
     """
 
     istream: Out(stream.Signature(data.ArrayLayout(ASQ, 4)))
@@ -85,6 +88,95 @@ class AudioStream(wiring.Component):
                     self.eurorack_pmod.sample_o.eq(dac_fifo.r_data),
                 ]
                 m.next = 'READ'
+
+        return m
+
+class EurorackPmodPeripheral(Peripheral, Elaboratable):
+
+    """
+    Extremely basic SoC peripheral for eurorack-pmod self-testing.
+    TODO: extend this to allow glitch-free audio streaming with a FIFO interface.
+    """
+
+    def __init__(self, *, pmod, **kwargs):
+
+        super().__init__()
+
+        self.pmod = pmod
+
+        # CSRs
+        bank                   = self.csr_bank()
+
+        # CODEC samples
+        # TODO: synchronize to audio clock domain.
+        # TODO: setattr breaks amaranth's name tracer for CSRs?
+
+        # raw ADC samples
+        self._sample_adc0 = bank.csr(16, "r")
+        self._sample_adc1 = bank.csr(16, "r")
+        self._sample_adc2 = bank.csr(16, "r")
+        self._sample_adc3 = bank.csr(16, "r")
+
+        # calibrated incoming samples
+        self._sample_i0   = bank.csr(16, "r")
+        self._sample_i1   = bank.csr(16, "r")
+        self._sample_i2   = bank.csr(16, "r")
+        self._sample_i3   = bank.csr(16, "r")
+
+        # calibrated outgoing samples
+        self._sample_o0   = bank.csr(16, "w")
+        self._sample_o1   = bank.csr(16, "w")
+        self._sample_o2   = bank.csr(16, "w")
+        self._sample_o3   = bank.csr(16, "w")
+
+        # continuous touch sensing
+        self._touch0      = bank.csr(8, "r")
+        self._touch1      = bank.csr(8, "r")
+        self._touch2      = bank.csr(8, "r")
+        self._touch3      = bank.csr(8, "r")
+        self._touch4      = bank.csr(8, "r")
+        self._touch5      = bank.csr(8, "r")
+        self._touch6      = bank.csr(8, "r")
+        self._touch7      = bank.csr(8, "r")
+
+        # Data from I2C peripherals on eurorack-pmod hardware.
+        self._jack             = bank.csr(8, "r")
+        self._eeprom_mfg       = bank.csr(8, "r")
+        self._eeprom_dev       = bank.csr(8, "r")
+        self._eeprom_serial    = bank.csr(32, "r")
+
+        # Peripheral bus
+        self._bridge    = self.bridge(data_width=32, granularity=8, alignment=2)
+        self.bus        = self._bridge.bus
+
+    def elaborate(self, platform):
+        m = Module()
+
+        m.submodules.bridge  = self._bridge
+
+        # Hook all pmod signals up to CSRs
+
+        for n in range(4):
+            m.submodules += FFSynchronizer(
+                    self.pmod.sample_adc[n], getattr(self, f"_sample_adc{n}").r_data, reset=0)
+            m.submodules += FFSynchronizer(
+                    self.pmod.sample_i[n], getattr(self, f"_sample_i{n}").r_data, reset=0)
+            with m.If(getattr(self, f"_sample_o{n}").w_stb):
+                # TODO proper sync
+                m.d.sync += self.pmod.sample_o[n].eq(getattr(self, f"_sample_o{n}").w_data)
+
+        for n in range(8):
+            m.submodules += FFSynchronizer(
+                    self.pmod.touch[n], getattr(self, f"_touch{n}").r_data, reset=0)
+
+        m.submodules += FFSynchronizer(
+                self.pmod.jack, self._jack.r_data, reset=0)
+        m.submodules += FFSynchronizer(
+                self.pmod.eeprom_mfg, self._eeprom_mfg.r_data, reset=0)
+        m.submodules += FFSynchronizer(
+                self.pmod.eeprom_dev, self._eeprom_dev.r_data, reset=0)
+        m.submodules += FFSynchronizer(
+                self.pmod.eeprom_serial, self._eeprom_serial.r_data, reset=0)
 
         return m
 
