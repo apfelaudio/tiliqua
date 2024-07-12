73c8477a6e7de8afda3d88c93f8f87c2c46d08c8
Sebastian Holzapfel <me@sebholzapfel.com>
2024-07-12 21:05:39 +0200

    cleanup vscope options

--- gateware/src/example_soc/fw/src/main.rs
+++ gateware/src/example_soc/fw/src/main.rs
@@ -10,9 +10,7 @@ use tiliqua_fw::Serial0;
 use tiliqua_fw::Timer0;
 use tiliqua_fw::I2c0;
 
-use micromath::F32Ext;
-
-use log::{info, error};
+use log::info;
 
 use riscv_rt::entry;
 
@@ -20,22 +18,14 @@ use embedded_hal::i2c::{I2c, Operation};
 
 use core::convert::TryInto;
 
-use fastrand;
-
 use embedded_graphics::{
-    mono_font::{ascii::FONT_6X10, ascii::FONT_9X15_BOLD, MonoTextStyle},
     pixelcolor::{Gray8, GrayColor},
     prelude::*,
-    text::{Alignment, Text},
 };
-
-use heapless::String;
-use core::fmt::Write;
 
 use tiliqua_fw::opts;
 use tiliqua_lib::draw;
 
-const TUSB322I_ADDR:  u8 = 0x47;
 const PCA9635_ADDR:   u8 = 0x05;
 
 // TODO: fetch these from SVF
@@ -44,189 +34,10 @@ const H_ACTIVE:       u32   = 800;
 const V_ACTIVE:       u32   = 600;
 
 // 16MiB, 4 bytes per word.
-const PSRAM_SZ_WORDS: usize = 1024 * 1024 * (16 / 4); 
-// 32-bit words in the framebuffer PSRAM section
-const _PSRAM_FB_BYTES: usize = (H_ACTIVE as usize * V_ACTIVE as usize) / 4;
-const _PSRAM_FB_WORDS: usize = _PSRAM_FB_BYTES / 4;
+const _PSRAM_SZ_WORDS: usize = 1024 * 1024 * (16 / 4); 
 const PSRAM_FB_BASE:  usize = PSRAM_BASE;
 
 tiliqua_hal::impl_dma_display!(DMADisplay, H_ACTIVE, V_ACTIVE);
-
-fn psram_memtest(timer: &mut Timer0) {
-
-    info!("PSRAM memtest (this will be slow if video is also active)...");
-
-    // WARN: assume framebuffer is at the start of PSRAM - don't try memtesting that section.
-
-    let psram_ptr = PSRAM_BASE as *mut u32;
-    let psram_sz_test = 1024*1024;
-
-    timer.enable();
-    timer.set_timeout_ticks(0xFFFFFFFF);
-
-    let start = timer.counter();
-
-    unsafe {
-        for i in (PSRAM_SZ_WORDS - psram_sz_test)..PSRAM_SZ_WORDS {
-            psram_ptr.offset(i as isize).write_volatile(i as u32);
-        }
-    }
-
-    pac::cpu::vexriscv::flush_dcache();
-
-    let endwrite = timer.counter();
-
-    unsafe {
-        for i in (PSRAM_SZ_WORDS - psram_sz_test)..PSRAM_SZ_WORDS {
-            let value = psram_ptr.offset(i as isize).read_volatile();
-            if (i as u32) != value {
-                error!("FAIL: PSRAM selftest @ {:#x} is {:#x}", i, value);
-                panic!();
-            }
-        }
-    }
-
-    let endread = timer.counter();
-
-    let write_ticks = start-endwrite;
-    let read_ticks = endwrite-endread;
-
-    let sysclk = pac::clock::sysclk();
-    info!("write speed {} KByte/sec", ((sysclk as u64) * (psram_sz_test/1024) as u64) / write_ticks as u64);
-    info!("read speed {} KByte/sec", ((sysclk as u64) * (psram_sz_test/1024) as u64) / (read_ticks as u64));
-
-    info!("PASS: PSRAM memtest");
-}
-
-fn tusb322i_id_test(i2cdev: &mut I2c0) {
-    info!("Read TUSB322I Device ID...");
-
-    // Read TUSB322I device ID
-    let mut tusb322i_id: [u8; 8] = [0; 8];
-    let _ = i2cdev.transaction(TUSB322I_ADDR, &mut [Operation::Write(&[0x00u8]),
-                                                    Operation::Read(&mut tusb322i_id)]);
-    if tusb322i_id != [0x32, 0x32, 0x33, 0x42, 0x53, 0x55, 0x54, 0x0] {
-        let mut ix = 0;
-        for byte in tusb322i_id {
-            info!("tusb322i_id{}: 0x{:x}", ix, byte);
-            ix += 1;
-        }
-        panic!("FAIL: TUSB322I ID");
-    }
-
-    info!("PASS: TUSB322I Device ID.");
-}
-
-fn print_encoder_state<D>(d: &mut D, rotation: i16, button: bool)
-where
-    D: DrawTarget<Color = Gray8>,
-{
-    let mut s = String::<64>::new();
-    write!(s, "encoder        - btn={} rot={}",
-          button,
-          rotation).ok();
-    info!("{}", s);
-    let style = MonoTextStyle::new(&FONT_6X10, Gray8::WHITE);
-    Text::with_alignment(
-        &s,
-        d.bounding_box().center() + Point::new(-140, -24),
-        style,
-        Alignment::Left,
-    )
-    .draw(d).ok();
-}
-
-fn print_codec_state<D>(d: &mut D, pmod: &pac::PMOD0_PERIPH)
-where
-    D: DrawTarget<Color = Gray8>,
-{
-    let mut s = String::<64>::new();
-    write!(s, "codec_raw_adc  - ch0={:06} ch1={:06} ch2={:06} ch3={:06}",
-          pmod.sample_adc0().read().bits() as i16,
-          pmod.sample_adc1().read().bits() as i16,
-          pmod.sample_adc2().read().bits() as i16,
-          pmod.sample_adc3().read().bits() as i16).ok();
-    info!("{}", s);
-    let style = MonoTextStyle::new(&FONT_6X10, Gray8::WHITE);
-    Text::with_alignment(
-        &s,
-        d.bounding_box().center() + Point::new(-140, -12),
-        style,
-        Alignment::Left,
-    )
-    .draw(d).ok();
-}
-
-fn print_touch_state<D>(d: &mut D, pmod: &pac::PMOD0_PERIPH)
-where
-    D: DrawTarget<Color = Gray8>,
-{
-    let mut s = String::<64>::new();
-    write!(s, "touch          - ch0={:03} ch1={:03} ch2={:03} ch3={:03} ch4={:03} ch5={:03} ch6={:03} ch7={:03}",
-          pmod.touch0().read().bits() as u8,
-          pmod.touch1().read().bits() as u8,
-          pmod.touch2().read().bits() as u8,
-          pmod.touch3().read().bits() as u8,
-          pmod.touch4().read().bits() as u8,
-          pmod.touch5().read().bits() as u8,
-          pmod.touch6().read().bits() as u8,
-          pmod.touch7().read().bits() as u8).ok();
-    info!("{}", s);
-    let style = MonoTextStyle::new(&FONT_6X10, Gray8::WHITE);
-    Text::with_alignment(
-        &s,
-        d.bounding_box().center() + Point::new(-140, 0),
-        style,
-        Alignment::Left,
-    )
-    .draw(d).ok();
-}
-
-fn print_usb_state<D>(d: &mut D, i2cdev: &mut I2c0)
-where
-    D: DrawTarget<Color = Gray8>,
-{
-    // Read TUSB322I connection status register
-    // We don't use this yet. But it's useful for checking for usb circuitry assembly problems.
-    // (in particular the cable orientation detection registers)
-    let mut tusb322_conn_status: [u8; 1] = [0; 1];
-    let _ = i2cdev.transaction(TUSB322I_ADDR, &mut [Operation::Write(&[0x09u8]),
-                                                    Operation::Read(&mut tusb322_conn_status)]);
-
-    let mut s = String::<64>::new();
-    write!(s, "tusb322i_conn  - 0x{:x} (DUA={} DDC={} VF={} IS={} CD={} AS={})",
-          tusb322_conn_status[0],
-          tusb322_conn_status[0]        & 0x1,
-          (tusb322_conn_status[0] >> 1) & 0x3,
-          (tusb322_conn_status[0] >> 3) & 0x1,
-          (tusb322_conn_status[0] >> 4) & 0x1,
-          (tusb322_conn_status[0] >> 5) & 0x1,
-          (tusb322_conn_status[0] >> 6) & 0x3,
-          ).ok();
-    info!("{}", s);
-    let style = MonoTextStyle::new(&FONT_6X10, Gray8::WHITE);
-    Text::with_alignment(
-        &s,
-        d.bounding_box().center() + Point::new(-140, 12),
-        style,
-        Alignment::Left,
-    )
-    .draw(d).ok();
-}
-
-fn print_tiliqua<D>(d: &mut D, rng: &mut fastrand::Rng)
-where
-    D: DrawTarget<Color = Gray8>,
-{
-    let style = MonoTextStyle::new(&FONT_9X15_BOLD, Gray8::WHITE);
-    Text::with_alignment(
-        "TILIQUA SELF TEST",
-        Point::new(rng.i32(0..H_ACTIVE as i32), rng.i32(0..V_ACTIVE as i32)),
-        style,
-        Alignment::Center,
-    )
-    .draw(d).ok();
-}
 
 #[entry]
 fn main() -> ! {
@@ -243,14 +54,9 @@ fn main() -> ! {
 
     info!("Hello from Tiliqua selftest!");
 
-    //psram_memtest(&mut timer);
-
     let mut i2cdev = I2c0::new(peripherals.I2C0);
 
-    tusb322i_id_test(&mut i2cdev);
-
     let encoder = peripherals.ENCODER0;
-    let pmod = peripherals.PMOD0_PERIPH;
 
     let mut display = DMADisplay {
         framebuffer_base: PSRAM_FB_BASE as *mut u32,
@@ -265,11 +71,10 @@ fn main() -> ! {
     };
 
     let mut uptime_ms = 0u32;
-    let mut period_ms = 10u32;
+    let period_ms = 10u32;
     let mut encoder_rotation: i16 = 0;
     let mut encoder_last = 0i16;
     let mut encoder_last_btn = false;
-    let mut rng = fastrand::Rng::with_seed(0);
 
     let mut opts = opts::Options::new();
 
@@ -281,18 +86,6 @@ fn main() -> ! {
 
         // Report encoder state
         encoder_rotation += (encoder.step().read().bits() as i8) as i16;
-
-        /*
-        // Make rotation control loop speed
-        if encoder_rotation > -25 {
-            period_ms = (25 + encoder_rotation) as u32;
-        }
-        print_tiliqua(&mut display, &mut rng);
-        pause_flush(&mut timer, &mut uptime_ms, period_ms);
-
-        print_encoder_state(&mut display, encoder_rotation, encoder.button().read().bits() != 0);
-        pause_flush(&mut timer, &mut uptime_ms, period_ms);
-        */
 
         draw::draw_options(&mut display, &opts, H_ACTIVE-200, V_ACTIVE-100, opts.xbeam.hue.value).ok();
 
@@ -326,36 +119,6 @@ fn main() -> ! {
         vs.intensity().write(|w| unsafe { w.intensity().bits(opts.xbeam.intensity.value) } );
 
         vs.decay().write(|w| unsafe { w.decay().bits(opts.xbeam.decay.value) } );
-
-
-        /*
-
-        // Report some eurorack-pmod information
-        print_codec_state(&mut display, &pmod);
-
-        pause_flush(&mut timer, &mut uptime_ms, period_ms);
-
-        // Report some touch sensing information
-        print_touch_state(&mut display, &pmod);
-
-        pause_flush(&mut timer, &mut uptime_ms, period_ms);
-
-        // Report some information about USB UFP/DFP + orientation
-        print_usb_state(&mut display, &mut i2cdev);
-
-        pause_flush(&mut timer, &mut uptime_ms, period_ms);
-
-        // Write something to the CODEC outputs / LEDs
-        pmod.sample_o0().write(|w| unsafe { w.sample_o0().bits(
-            ((f32::sin((uptime_ms as f32)/200.0f32 + 0.0) * 16000.0f32) as i16) as u16) } );
-        pmod.sample_o1().write(|w| unsafe { w.sample_o1().bits(
-            ((f32::sin((uptime_ms as f32)/200.0f32 + 1.0) * 16000.0f32) as i16) as u16) } );
-        pmod.sample_o2().write(|w| unsafe { w.sample_o2().bits(
-            ((f32::sin((uptime_ms as f32)/200.0f32 + 2.0) * 16000.0f32) as i16) as u16) } );
-        pmod.sample_o3().write(|w| unsafe { w.sample_o3().bits(
-            ((f32::sin((uptime_ms as f32)/200.0f32 + 3.0) * 16000.0f32) as i16) as u16) } );
-
-        */
 
         // Write something interesting to the LED expander
         let pca9635_bytes = [
--- gateware/src/example_soc/fw/src/opts.rs
+++ gateware/src/example_soc/fw/src/opts.rs
@@ -9,25 +9,9 @@ use core::str::FromStr;
 use strum_macros::{EnumIter, IntoStaticStr};
 
 #[derive(Clone, Copy, PartialEq, EnumIter, IntoStaticStr)]
-#[strum(serialize_all = "kebab-case")]
-pub enum TouchLedMirror {
-    MirrorOff,
-    MirrorOn,
-}
-
-#[derive(Clone, Copy, PartialEq, EnumIter, IntoStaticStr)]
-#[strum(serialize_all = "kebab-case")]
-pub enum NoteControl {
-    Touch,
-    Midi,
-}
-
-#[derive(Clone, Copy, PartialEq, EnumIter, IntoStaticStr)]
 #[strum(serialize_all = "SCREAMING-KEBAB-CASE")]
 pub enum Screen {
     Xbeam,
-    Scope,
-    Touch,
 }
 
 #[derive(Clone)]
@@ -39,30 +23,8 @@ pub struct XbeamOptions {
     pub decay: NumOption<u8>,
 }
 
-#[derive(Clone)]
-pub struct ScopeOptions {
-    pub selected: Option<usize>,
-    pub grain_sz: NumOption<u32>,
-    pub trig_lvl: NumOption<i32>,
-    pub trig_sns: NumOption<i32>,
-}
-
-#[derive(Clone)]
-pub struct TouchOptions {
-    pub selected: Option<usize>,
-    pub note_control: EnumOption<NoteControl>,
-    pub led_mirror: EnumOption<TouchLedMirror>,
-}
-
 impl_option_view!(XbeamOptions,
                   persist, hue, intensity, decay);
-
-impl_option_view!(ScopeOptions,
-                  grain_sz, trig_lvl, trig_sns);
-
-impl_option_view!(TouchOptions,
-                  note_control, led_mirror);
-
 
 #[derive(Clone)]
 pub struct Options {
@@ -70,14 +32,10 @@ pub struct Options {
     pub screen: EnumOption<Screen>,
 
     pub xbeam: XbeamOptions,
-    pub scope: ScopeOptions,
-    pub touch: TouchOptions,
 }
 
 impl_option_page!(Options,
-                  (Screen::Xbeam, xbeam),
-                  (Screen::Scope, scope),
-                  (Screen::Touch, touch));
+                  (Screen::Xbeam, xbeam));
 
 impl Options {
     pub fn new() -> Options {
@@ -118,41 +76,6 @@ impl Options {
                     max: 15,
                 },
             },
-            scope: ScopeOptions {
-                selected: None,
-                grain_sz: NumOption{
-                    name: String::from_str("grainsz").unwrap(),
-                    value: 1000,
-                    step: 1,
-                    min: 512,
-                    max: 1000,
-                },
-                trig_lvl: NumOption{
-                    name: String::from_str("trig lvl").unwrap(),
-                    value: 0,
-                    step: 100,
-                    min: -10000,
-                    max: 10000,
-                },
-                trig_sns: NumOption{
-                    name: String::from_str("trig sns").unwrap(),
-                    value: 1000,
-                    step: 100,
-                    min: 100,
-                    max: 5000,
-                },
-            },
-            touch: TouchOptions {
-                selected: None,
-                note_control: EnumOption{
-                    name: String::from_str("control").unwrap(),
-                    value: NoteControl::Touch,
-                },
-                led_mirror: EnumOption{
-                    name: String::from_str("led").unwrap(),
-                    value: TouchLedMirror::MirrorOn,
-                },
-            }
         }
     }
 }
