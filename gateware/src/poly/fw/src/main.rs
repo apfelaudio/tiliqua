01801e19c5679ca93e6963757743f046fe959fdd
Sebastian Holzapfel <me@sebholzapfel.com>
2024-08-07 17:08:48 +0200

    polybeam runs

---
+++ gateware/src/poly/fw/src/main.rs
@@ -0,0 +1,177 @@
+#![no_std]
+#![no_main]
+
+use tiliqua_pac as pac;
+use tiliqua_hal as hal;
+
+use hal::hal::delay::DelayNs;
+
+use tiliqua_fw::Serial0;
+use tiliqua_fw::Timer0;
+use tiliqua_fw::I2c0;
+use tiliqua_fw::Encoder0;
+use tiliqua_fw::EurorackPmod0;
+
+use log::info;
+
+use riscv_rt::entry;
+
+use tiliqua_hal::pca9635::*;
+
+use core::convert::TryInto;
+
+use embedded_graphics::{
+    pixelcolor::{Gray8, GrayColor},
+    prelude::*,
+};
+
+use tiliqua_fw::opts;
+use tiliqua_lib::draw;
+
+use tiliqua_lib::opt::*;
+
+use tiliqua_lib::generated_constants::*;
+
+tiliqua_hal::impl_dma_display!(DMADisplay, H_ACTIVE, V_ACTIVE);
+
+const PCA9635_BAR_GREEN: [usize; 6] = [0, 2, 14, 12, 6, 4];
+const PCA9635_BAR_RED:   [usize; 6] = [1, 3, 15, 13, 7, 5];
+const _PCA9635_MIDI:     [usize; 2] = [8, 9];
+
+#[entry]
+fn main() -> ! {
+    let peripherals = pac::Peripherals::take().unwrap();
+
+    // initialize logging
+    let serial = Serial0::new(peripherals.UART);
+    tiliqua_fw::handlers::logger_init(serial);
+
+    let sysclk = pac::clock::sysclk();
+    let mut timer = Timer0::new(peripherals.TIMER, sysclk);
+
+    info!("Hello from Tiliqua XBEAM!");
+
+    let i2cdev = I2c0::new(peripherals.I2C0);
+
+    let mut pca9635 = Pca9635Driver::new(i2cdev);
+
+    let mut encoder = Encoder0::new(peripherals.ENCODER0);
+
+    let mut display = DMADisplay {
+        framebuffer_base: PSRAM_FB_BASE as *mut u32,
+    };
+
+    // Must flush the dcache for framebuffer writes to go through
+    // TODO: put the framebuffer in the DMA section of Vex address space?
+    let pause_flush = |timer: &mut Timer0, uptime_ms: &mut u32, period_ms: u32| {
+        timer.delay_ms(period_ms);
+        *uptime_ms += period_ms;
+        pac::cpu::vexriscv::flush_dcache();
+    };
+
+    let mut uptime_ms = 0u32;
+    let period_ms = 10u32;
+
+    let mut opts = opts::Options::new();
+
+    let vs = peripherals.VS_PERIPH;
+
+    let mut pmod = EurorackPmod0::new(peripherals.PMOD0_PERIPH);
+
+    let mut toggle_encoder_leds = false;
+
+    let mut time_since_encoder_touched: u32 = 0;
+
+    loop {
+
+        if time_since_encoder_touched < 1000 || opts.modify() {
+
+            draw::draw_options(&mut display, &opts, H_ACTIVE-200, V_ACTIVE-100, opts.xbeam.hue.value).ok();
+
+        }
+
+        pause_flush(&mut timer, &mut uptime_ms, period_ms);
+
+        encoder.update();
+
+        time_since_encoder_touched += period_ms;
+
+        match encoder.poke_ticks() {
+            1 => {
+                opts.tick_up();
+                time_since_encoder_touched = 0;
+            }
+            -1 => {
+                opts.tick_down();
+                time_since_encoder_touched = 0;
+            }
+            _ => {},
+        }
+
+        if encoder.poke_btn() {
+            opts.toggle_modify();
+            time_since_encoder_touched = 0;
+        }
+
+        vs.persist().write(|w| unsafe { w.persist().bits(opts.xbeam.persist.value) } );
+        vs.hue().write(|w| unsafe { w.hue().bits(opts.xbeam.hue.value) } );
+        vs.intensity().write(|w| unsafe { w.intensity().bits(opts.xbeam.intensity.value) } );
+        vs.decay().write(|w| unsafe { w.decay().bits(opts.xbeam.decay.value) } );
+        vs.scale().write(|w| unsafe { w.scale().bits(opts.xbeam.scale.value) } );
+
+        for n in 0..16 {
+            pca9635.leds[n] = 0u8;
+        }
+
+        if uptime_ms % 50 == 0 {
+            toggle_encoder_leds = !toggle_encoder_leds;
+        }
+
+        if let Some(n) = opts.view().selected() {
+            let o = opts.view().options()[n];
+            let c = o.percent();
+            for n in 0..6 {
+                if ((n as f32)*0.5f32/6.0f32 + 0.5) < c {
+                    pca9635.leds[PCA9635_BAR_RED[n]] = 0xff as u8;
+                } else {
+                    pca9635.leds[PCA9635_BAR_RED[n]] = 0 as u8;
+                }
+                if ((n as f32)*-0.5f32/6.0f32 + 0.5) > c {
+                    pca9635.leds[PCA9635_BAR_GREEN[n]] = 0xff as u8;
+                } else {
+                    pca9635.leds[PCA9635_BAR_GREEN[n]] = 0 as u8;
+                }
+            }
+
+            if opts.modify() && !toggle_encoder_leds {
+                for n in 0..6 {
+                    pca9635.leds[PCA9635_BAR_GREEN[n]] = 0 as u8;
+                    pca9635.leds[PCA9635_BAR_RED[n]] = 0 as u8;
+                }
+            }
+        }
+
+        if opts.modify() {
+            if toggle_encoder_leds {
+                if let Some(n) = opts.view().selected() {
+                    pmod.led_set_manual(n, i8::MAX);
+                }
+            } else {
+                pmod.led_all_auto();
+            }
+        } else {
+            if time_since_encoder_touched < 1000 {
+                for n in 0..8 {
+                    pmod.led_set_manual(n, 0i8);
+                }
+                if let Some(n) = opts.view().selected() {
+                    pmod.led_set_manual(n, (((1000-time_since_encoder_touched) * 120) / 1000) as i8);
+                }
+            } else {
+                pmod.led_all_auto();
+            }
+        }
+
+        pca9635.push().ok();
+    }
+}
---
+++ gateware/src/poly/top.py
@@ -0,0 +1,393 @@
+# Copyright (c) 2024 Seb Holzapfel, apfelaudio UG <info@apfelaudio.com>
+#
+# SPDX-License-Identifier: CERN-OHL-S-2.0
+
+import logging
+import os
+import sys
+import math
+
+from amaranth                                    import *
+from amaranth.hdl.rec                            import Record
+from amaranth.lib                                import wiring, data
+from amaranth.lib.wiring                         import In, Out
+
+from amaranth_future                             import stream, fixed
+
+from luna_soc.util.readbin                       import get_mem_data
+from luna_soc                                    import top_level_cli
+from luna_soc.gateware.csr.base                  import Peripheral
+
+from tiliqua                                     import eurorack_pmod, dsp, midi
+from tiliqua.eurorack_pmod                       import ASQ
+from tiliqua.tiliqua_platform                    import TiliquaPlatform, set_environment_variables
+from tiliqua.tiliqua_soc                         import TiliquaSoc
+
+from example_vectorscope.top                     import Stroke
+
+class Diffuser(wiring.Component):
+
+    """
+    4-channel feedback delay, diffused by a matrix mixer.
+    """
+
+    i: In(stream.Signature(data.ArrayLayout(ASQ, 4)))
+    o: Out(stream.Signature(data.ArrayLayout(ASQ, 4)))
+
+    def elaborate(self, platform):
+        m = Module()
+
+        # quadrants in the below matrix are:
+        #
+        # [in    -> out] [in    -> delay]
+        # [delay -> out] [delay -> delay] <- feedback
+        #
+
+        m.submodules.matrix_mix = matrix_mix = dsp.MatrixMix(
+            i_channels=8, o_channels=8,
+            coefficients=[[0.6, 0.0, 0.0, 0.0, 0.8, 0.0, 0.0, 0.0], # in0
+                          [0.0, 0.6, 0.0, 0.0, 0.0, 0.8, 0.0, 0.0], #  |
+                          [0.0, 0.0, 0.6, 0.0, 0.0, 0.0, 0.8, 0.0], #  |
+                          [0.0, 0.0, 0.0, 0.6, 0.0, 0.0, 0.0, 0.8], # in3
+                          [0.4, 0.0, 0.0, 0.0, 0.4,-0.4,-0.4,-0.4], # ds0
+                          [0.0, 0.4, 0.0, 0.0,-0.4, 0.4,-0.4,-0.4], #  |
+                          [0.0, 0.0, 0.4, 0.0,-0.4,-0.4, 0.4,-0.4], #  |
+                          [0.0, 0.0, 0.0, 0.4,-0.4,-0.4,-0.4, 0.4]])# ds3
+                          # out0 ------- out3  sw0 ---------- sw3
+
+        delay_lines = [
+            dsp.DelayLine(max_delay=2048),
+            dsp.DelayLine(max_delay=4096),
+            dsp.DelayLine(max_delay=8192),
+            dsp.DelayLine(max_delay=8192),
+        ]
+        m.submodules += delay_lines
+
+        m.d.comb += [delay_lines[n].da.valid.eq(1) for n in range(4)]
+        m.d.comb += [
+            delay_lines[0].da.payload.eq(2000),
+            delay_lines[1].da.payload.eq(3000),
+            delay_lines[2].da.payload.eq(5000),
+            delay_lines[3].da.payload.eq(7000),
+        ]
+
+        m.submodules.split4 = split4 = dsp.Split(n_channels=4)
+        m.submodules.merge4 = merge4 = dsp.Merge(n_channels=4)
+
+        m.submodules.split8 = split8 = dsp.Split(n_channels=8)
+        m.submodules.merge8 = merge8 = dsp.Merge(n_channels=8)
+
+        wiring.connect(m, wiring.flipped(self.i), split4.i)
+
+        # matrix <-> independent streams
+        wiring.connect(m, matrix_mix.o, split8.i)
+        wiring.connect(m, merge8.o, matrix_mix.i)
+
+        for n in range(4):
+            # audio -> matrix [0-3]
+            wiring.connect(m, split4.o[n], merge8.i[n])
+            # delay -> matrix [4-7]
+            wiring.connect(m, delay_lines[n].ds, merge8.i[4+n])
+
+        for n in range(4):
+            # matrix -> audio [0-3]
+            wiring.connect(m, split8.o[n], merge4.i[n])
+            # matrix -> delay [4-7]
+            wiring.connect(m, split8.o[4+n], delay_lines[n].sw)
+
+        wiring.connect(m, merge4.o, wiring.flipped(self.o))
+
+        return m
+
+class PolySynth(wiring.Component):
+
+    i: In(stream.Signature(data.ArrayLayout(ASQ, 4)))
+    o: Out(stream.Signature(data.ArrayLayout(ASQ, 4)))
+
+    i_midi: In(stream.Signature(midi.MidiMessage))
+
+    def elaborate(self, platform):
+        m = Module()
+
+        # supported simultaneous voices
+        n_voices = 8
+
+        # Create LUTs from midi note to freq_inc (ASQ tuning into NCO).
+        # Store it in memories where the address is the midi note,
+        # and the data coming out is directly routed to NCO freq_inc.
+        lut = []
+        sample_rate_hz = 48000
+        for i in range(128):
+            freq = 440 * 2**((i-69)/12.0)
+            freq_inc = freq * (1.0 / sample_rate_hz)
+            lut.append(fixed.Const(freq_inc, shape=ASQ)._value)
+        mems = [Memory(width=ASQ.as_shape().width, depth=len(lut), init=lut)
+                for _ in range(n_voices)]
+        rports = [mems[n].read_port(transparent=True) for n in range(n_voices)]
+        m.submodules += mems
+
+        voice_tracker = midi.MidiVoiceTracker(max_voices=n_voices)
+        # 1 smoother per oscillator for filter cutoff, to prevent pops.
+        boxcars = [dsp.Boxcar(n=16) for _ in range(n_voices)]
+        # 1 oscillator and filter per oscillator
+        ncos = [dsp.SawNCO(shift=0) for _ in range(n_voices)]
+        svfs = [dsp.SVF() for _ in range(n_voices)]
+        merge = dsp.Merge(n_channels=n_voices)
+        m.submodules += [voice_tracker, boxcars, ncos, svfs, merge]
+
+        # Connect MIDI stream -> voice tracker
+        wiring.connect(m, wiring.flipped(self.i_midi), voice_tracker.i)
+
+        # Use CC1 (mod wheel) as upper bound on filter cutoff.
+        last_cc1 = Signal(8, reset=255)
+        with m.If(self.i_midi.valid):
+            msg = self.i_midi.payload
+            with m.Switch(msg.midi_type):
+                with m.Case(midi.MessageType.CONTROL_CHANGE):
+                    # mod wheel is CC 1
+                    with m.If(msg.midi_payload.control_change.controller_number == 1):
+                        m.d.sync += last_cc1.eq(msg.midi_payload.control_change.data)
+
+        # analog ins
+        m.submodules.cv_in = cv_in = dsp.Split(
+                n_channels=4, source=wiring.flipped(self.i))
+        cv_in.wire_ready(m, [2, 3])
+
+        for n in range(n_voices):
+
+            # Filter cutoff on all channels is min(mod wheel, note velocity)
+            # Cutoff itself is smoothed by boxcars before being sent to SVF cutoff.
+            with m.If(last_cc1 < voice_tracker.o[n].payload.velocity):
+                m.d.comb += boxcars[n].i.payload.sas_value().eq(last_cc1 << 4)
+            with m.Else():
+                m.d.comb += boxcars[n].i.payload.sas_value().eq(
+                        voice_tracker.o[n].payload.velocity << 4)
+
+            m.d.comb += [
+                # Connect voice.note -> note to frequency LUT
+                rports[n].en.eq(1),
+                rports[n].addr.eq(voice_tracker.o[n].payload.note),
+            ]
+
+            # Connect LUT output -> NCO.i (clocked at i.valid for normal sample rate)
+            dsp.connect_remap(m, cv_in.o[0], ncos[n].i, lambda o, i : [
+                # For fun, phase mod on audio in #0
+                i.payload.phase   .eq(o.payload),
+                i.payload.freq_inc.eq(rports[n].data) # ok, always valid
+            ])
+
+            # Connect voice.vel and NCO.o -> SVF.i
+            dsp.connect_remap(m, ncos[n].o, svfs[n].i, lambda o, i : [
+                i.payload.x                    .eq(o.payload >> 1),
+                i.payload.resonance.sas_value().eq(8000),
+                i.payload.cutoff               .eq(boxcars[n].o.payload) # hack
+            ])
+
+            # Connect SVF LPF -> merge channel
+            dsp.connect_remap(m, svfs[n].o, merge.i[n], lambda o, i : [
+                i.payload.eq(o.payload.lp),
+            ])
+
+            # HACK: Boxcar synchronization
+            m.d.comb += [
+                boxcars[n].i.valid.eq(ncos[n].o.valid),
+                boxcars[n].o.ready.eq(svfs[n].i.ready),
+            ]
+
+        # Voice mixdown to stereo. Alternate left/right
+        o_channels = 2
+        coefficients = [[0.75*o_channels/n_voices, 0.0                ],
+                        [0.0,                      0.75*o_channels/n_voices]] * (n_voices // 2)
+        m.submodules.matrix_mix = matrix_mix = dsp.MatrixMix(
+            i_channels=n_voices, o_channels=o_channels,
+            coefficients=coefficients)
+        wiring.connect(m, merge.o, matrix_mix.i),
+
+        # Output diffuser
+
+        m.submodules.diffuser = diffuser = Diffuser()
+
+        # Stereo HPF to remove DC from any voices in 'zero cutoff'
+        # Route to audio output channels 2 & 3
+
+        output_hpfs = [dsp.SVF() for _ in range(o_channels)]
+        m.submodules += output_hpfs
+
+        m.submodules.hpf_split2 = hpf_split2 = dsp.Split(n_channels=2, source=matrix_mix.o)
+        m.submodules.hpf_merge4 = hpf_merge4 = dsp.Merge(n_channels=4, sink=diffuser.i)
+        hpf_merge4.wire_valid(m, [0, 1])
+
+        for lr in [0, 1]:
+            dsp.connect_remap(m, hpf_split2.o[lr], output_hpfs[lr].i, lambda o, i : [
+                i.payload.x                     .eq(o.payload),
+                i.payload.cutoff.sas_value()    .eq(200),
+                i.payload.resonance.sas_value() .eq(20000),
+            ])
+
+            dsp.connect_remap(m, output_hpfs[lr].o, hpf_merge4.i[2+lr], lambda o, i : [
+                i.payload.eq(o.payload.hp << 2)
+            ])
+
+        # Implement stereo distortion effect after diffuser.
+
+        m.submodules.diffuser_split4 = diffuser_split4 = dsp.Split(
+                n_channels=4, source=diffuser.o)
+        diffuser_split4.wire_ready(m, [0, 1])
+
+        m.submodules.cv_gain_split2 = cv_gain_split2 = dsp.Split(
+                n_channels=2, replicate=True, source=cv_in.o[1])
+
+        def scaled_tanh(x):
+            return math.tanh(3.0*x)
+
+        outs = []
+        for lr in [0, 1]:
+            vca = dsp.GainVCA()
+            waveshaper = dsp.WaveShaper(lut_function=scaled_tanh)
+            vca_merge2 = dsp.Merge(n_channels=2)
+            m.submodules += [vca, waveshaper, vca_merge2]
+
+            wiring.connect(m, diffuser_split4.o[2+lr], vca_merge2.i[0])
+            wiring.connect(m, cv_gain_split2.o[lr],    vca_merge2.i[1])
+
+            dsp.connect_remap(m, vca_merge2.o, vca.i, lambda o, i : [
+                i.payload.x   .eq(o.payload[0]),
+                i.payload.gain.eq(o.payload[1] << 2)
+            ])
+
+            wiring.connect(m, vca.o, waveshaper.i)
+            outs.append(waveshaper.o)
+
+        # Final outputs on channel 2, 3
+        m.submodules.merge4 = merge4 = dsp.Merge(
+                n_channels=4, sink=wiring.flipped(self.o))
+        merge4.wire_valid(m, [0, 1])
+        wiring.connect(m, outs[0], merge4.i[2])
+        wiring.connect(m, outs[1], merge4.i[3])
+
+        return m
+
+
+class VSPeripheral(Peripheral, Elaboratable):
+
+    """
+    Placeholder peripheral that bridges SoC memory space such that
+    we can tweak vectorscope properties from Rust.
+    """
+
+    def __init__(self):
+
+        super().__init__()
+
+        self.persist           = Signal(16, reset=1024)
+        self.hue               = Signal(8,  reset=0)
+        self.intensity         = Signal(8,  reset=4)
+        self.decay             = Signal(8,  reset=1)
+        self.scale             = Signal(8,  reset=6)
+
+        # CSRs
+        bank                   = self.csr_bank()
+        self._persist          = bank.csr(16, "w")
+        self._hue              = bank.csr(8, "w")
+        self._intensity        = bank.csr(8, "w")
+        self._decay            = bank.csr(8, "w")
+        self._scale            = bank.csr(8, "w")
+
+        # Peripheral bus
+        self._bridge    = self.bridge(data_width=32, granularity=8, alignment=2)
+        self.bus        = self._bridge.bus
+
+    def elaborate(self, platform):
+        m = Module()
+
+        m.submodules.bridge  = self._bridge
+
+        with m.If(self._persist.w_stb):
+            m.d.sync += self.persist.eq(self._persist.w_data)
+
+        with m.If(self._hue.w_stb):
+            m.d.sync += self.hue.eq(self._hue.w_data)
+
+        with m.If(self._intensity.w_stb):
+            m.d.sync += self.intensity.eq(self._intensity.w_data)
+
+        with m.If(self._decay.w_stb):
+            m.d.sync += self.decay.eq(self._decay.w_data)
+
+        with m.If(self._scale.w_stb):
+            m.d.sync += self.scale.eq(self._scale.w_data)
+
+        return m
+
+class PolySoc(TiliquaSoc):
+    def __init__(self, *, firmware_path, dvi_timings):
+        super().__init__(firmware_path=firmware_path, dvi_timings=dvi_timings, audio_192=False,
+                         audio_out_peripheral=False)
+        # scope stroke bridge from audio stream
+        fb_size = (self.video.fb_hsize, self.video.fb_vsize)
+        self.stroke = Stroke(
+                fb_base=self.video.fb_base, bus_master=self.soc.psram.bus, fb_size=fb_size)
+        self.soc.psram.add_master(self.stroke.bus)
+        # scope controls
+        self.vs_periph = VSPeripheral()
+        self.soc.add_peripheral(self.vs_periph, addr=0xf0006000)
+
+    def elaborate(self, platform):
+
+        m = Module()
+
+        m.submodules += super().elaborate(platform)
+
+        pmod0 = self.pmod0_periph.pmod
+
+        m.submodules.astream = astream = eurorack_pmod.AudioStream(pmod0)
+
+        m.submodules.stroke = self.stroke
+
+        # Memory controller hangs if we start making requests to it straight away.
+        # TODO collapse this into delay already present in super()
+        on_delay = Signal(32)
+        with m.If(on_delay < 0xFFFF):
+            m.d.sync += on_delay.eq(on_delay+1)
+        with m.Else():
+            m.d.sync += self.stroke.enable.eq(1)
+
+        m.d.comb += [
+            self.persist.holdoff.eq(self.vs_periph.persist),
+            self.persist.decay.eq(self.vs_periph.decay),
+            self.stroke.hue.eq(self.vs_periph.hue),
+            self.stroke.intensity.eq(self.vs_periph.intensity),
+            self.stroke.scale.eq(self.vs_periph.scale),
+        ]
+
+        m.submodules.polysynth = polysynth = PolySynth()
+
+        # polysynth midi
+        midi_pins = platform.request("midi")
+        m.submodules.serialrx = serialrx = midi.SerialRx(
+                system_clk_hz=60e6, pins=midi_pins)
+        m.submodules.midi_decode = midi_decode = midi.MidiDecode()
+        wiring.connect(m, serialrx.o, midi_decode.i)
+        wiring.connect(m, midi_decode.o, polysynth.i_midi)
+
+        # polysynth audio
+        wiring.connect(m, astream.istream, polysynth.i)
+        wiring.connect(m, polysynth.o, astream.ostream)
+
+        # polysynth out -> vectorscope TODO use true split
+        m.d.comb += [
+            self.stroke.i.valid.eq(polysynth.o.valid),
+            self.stroke.i.payload[0].eq(polysynth.o.payload[2]),
+            self.stroke.i.payload[1].eq(polysynth.o.payload[3]),
+        ]
+
+        return m
+
+
+if __name__ == "__main__":
+    dvi_timings = set_environment_variables()
+    this_directory = os.path.dirname(os.path.realpath(__file__))
+    design = PolySoc(firmware_path=os.path.join(this_directory, "fw/firmware.bin"),
+                     dvi_timings=dvi_timings)
+    design.genrust_constants()
+    top_level_cli(design)
