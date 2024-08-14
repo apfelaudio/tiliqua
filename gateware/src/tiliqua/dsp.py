39997af6cc54f2bfb4ceb34a7d593afc023bd892
Sebastian Holzapfel <me@sebholzapfel.com>
2024-08-14 10:47:46 +0200

    scope attempt v2 -> test on non-soc vscope

--- gateware/src/tiliqua/dsp.py
+++ gateware/src/tiliqua/dsp.py
@@ -229,6 +229,81 @@ class SawNCO(wiring.Component):
 
         return m
 
+class Trigger(wiring.Component):
+
+    """
+    When trigger condition is met, output is set to 1, for 1 stream cycle.
+
+    Currently this only implements rising edge trigger.
+    """
+
+    i: In(stream.Signature(data.StructLayout({
+            "sample":    ASQ,
+            "threshold": ASQ,
+        })))
+    o: Out(stream.Signature(unsigned(1)))
+
+    def elaborate(self, platform):
+        m = Module()
+
+        trigger = Signal()
+        l_sample = Signal(shape=ASQ)
+
+        m.d.comb += [
+            self.o.valid.eq(self.i.valid),
+            self.i.ready.eq(self.o.ready),
+        ]
+
+        with m.If(self.i.valid & self.o.ready):
+            m.d.sync += l_sample.eq(self.i.payload.sample)
+            m.d.comb += [
+                self.o.payload.eq(
+                    (l_sample              < self.i.payload.threshold) &
+                    (self.i.payload.sample >= self.i.payload.threshold)
+                ),
+            ]
+
+        return m
+
+class Ramp(wiring.Component):
+
+    """
+    If trigger strobes a 1, ramps from -1 to 1, staying at 1 until retriggered.
+    A retrigger mid-ramp does not restart the ramp until the output has reached 1.
+    """
+
+    i: In(stream.Signature(data.StructLayout({
+            "trigger":  unsigned(1),
+            "td":       ASQ, # time delta
+        })))
+    o: Out(stream.Signature(ASQ))
+
+    def __init__(self, extra_bits=16, shift=6):
+        self.extra_bits = extra_bits
+        self.shift = shift
+        super().__init__()
+
+    def elaborate(self, platform):
+        m = Module()
+
+        s = Signal(fixed.SQ(self.extra_bits, ASQ.f_width))
+
+        m.d.comb += [
+            self.o.valid.eq(self.i.valid),
+            self.i.ready.eq(self.o.ready),
+            self.o.payload.eq(s >> self.shift),
+        ]
+
+        with m.If(self.i.valid & self.o.ready):
+            with m.If((self.o.payload > fixed.Const(0.95, shape=ASQ)) &
+                      (self.o.payload.as_value()[15] == 0)):
+                with m.If(self.i.payload.trigger):
+                    m.d.sync += s.eq(ASQ.min() << self.shift)
+            with m.Else():
+                m.d.sync += s.eq(s + self.i.payload.td)
+
+        return m
+
 class WaveShaper(wiring.Component):
 
     """
--- gateware/src/tiliqua/raster.py
+++ gateware/src/tiliqua/raster.py
@@ -197,7 +197,8 @@ class Stroke(wiring.Component):
     # x, y, intensity, color
     i: In(stream.Signature(data.ArrayLayout(ASQ, 4)))
 
-    def __init__(self, *, fb_base, bus_master, fb_size, fb_bytes_per_pixel=1, fs=192000, n_upsample=4):
+    def __init__(self, *, fb_base, bus_master, fb_size, fb_bytes_per_pixel=1, fs=192000, n_upsample=4,
+                 default_hue=10, default_x=0, default_y=0):
 
         self.fb_base = fb_base
         self.fb_hsize, self.fb_vsize = fb_size
@@ -213,9 +214,12 @@ class Stroke(wiring.Component):
         self.sample_p = Signal(signed(16)) # intensity modulation TODO
         self.sample_c = Signal(signed(16)) # color modulation DONE
 
-        self.hue       = Signal(4, reset=10); # default blue :)
+        self.hue       = Signal(4, reset=default_hue);
         self.intensity = Signal(4, reset=8);
-        self.scale     = Signal(4, reset=6);
+        self.scale_x   = Signal(4, reset=6);
+        self.scale_y   = Signal(4, reset=6);
+        self.x_offset  = Signal(signed(16), reset=default_x)
+        self.y_offset  = Signal(signed(16), reset=default_y)
 
         self.px_read = Signal(32)
         self.px_sum = Signal(16)
@@ -238,26 +242,32 @@ class Stroke(wiring.Component):
         sample_p = self.sample_p
         sample_c = self.sample_c
 
-        m.submodules.split = split = dsp.Split(n_channels=4)
-        m.submodules.merge = merge = dsp.Merge(n_channels=4)
+        point_stream = None
+        if self.n_upsample is not None and self.n_upsample != 1:
+            # If interpolation is enabled, insert an FIR upsampling stage.
+            m.submodules.split = split = dsp.Split(n_channels=4)
+            m.submodules.merge = merge = dsp.Merge(n_channels=4)
 
-        m.submodules.resample0 = resample0 = dsp.Resample(fs_in=self.fs, n_up=self.n_upsample, m_down=1)
-        m.submodules.resample1 = resample1 = dsp.Resample(fs_in=self.fs, n_up=self.n_upsample, m_down=1)
-        m.submodules.resample2 = resample2 = dsp.Resample(fs_in=self.fs, n_up=self.n_upsample, m_down=1)
-        m.submodules.resample3 = resample3 = dsp.Resample(fs_in=self.fs, n_up=self.n_upsample, m_down=1)
+            m.submodules.resample0 = resample0 = dsp.Resample(fs_in=self.fs, n_up=self.n_upsample, m_down=1)
+            m.submodules.resample1 = resample1 = dsp.Resample(fs_in=self.fs, n_up=self.n_upsample, m_down=1)
+            m.submodules.resample2 = resample2 = dsp.Resample(fs_in=self.fs, n_up=self.n_upsample, m_down=1)
+            m.submodules.resample3 = resample3 = dsp.Resample(fs_in=self.fs, n_up=self.n_upsample, m_down=1)
 
-        wiring.connect(m, wiring.flipped(self.i), split.i)
+            wiring.connect(m, wiring.flipped(self.i), split.i)
 
-        wiring.connect(m, split.o[0], resample0.i)
-        wiring.connect(m, split.o[1], resample1.i)
-        wiring.connect(m, split.o[2], resample2.i)
-        wiring.connect(m, split.o[3], resample3.i)
+            wiring.connect(m, split.o[0], resample0.i)
+            wiring.connect(m, split.o[1], resample1.i)
+            wiring.connect(m, split.o[2], resample2.i)
+            wiring.connect(m, split.o[3], resample3.i)
 
-        wiring.connect(m, resample0.o, merge.i[0])
-        wiring.connect(m, resample1.o, merge.i[1])
-        wiring.connect(m, resample2.o, merge.i[2])
-        wiring.connect(m, resample3.o, merge.i[3])
+            wiring.connect(m, resample0.o, merge.i[0])
+            wiring.connect(m, resample1.o, merge.i[1])
+            wiring.connect(m, resample2.o, merge.i[2])
+            wiring.connect(m, resample3.o, merge.i[3])
 
+            point_stream=merge.o
+        else:
+            point_stream=self.i
 
         px_read = self.px_read
         px_sum = self.px_sum
@@ -283,16 +293,14 @@ class Stroke(wiring.Component):
 
             with m.State('LATCH0'):
 
-                m.d.comb += merge.o.ready.eq(1)
+                m.d.comb += point_stream.ready.eq(1)
                 # Fired on every audio sample fs_strobe
-                with m.If(merge.o.valid):
+                with m.If(point_stream.valid):
                     m.d.sync += [
-                        # TODO this >>6 scales input -> screen mapping.
-                        # should be better exposed for tweaking.
-                        sample_x.eq(merge.o.payload[0].sas_value()>>self.scale),
-                        sample_y.eq(merge.o.payload[1].sas_value()>>self.scale),
-                        sample_p.eq(merge.o.payload[2].sas_value()),
-                        sample_c.eq(merge.o.payload[3].sas_value()),
+                        sample_x.eq((point_stream.payload[0].sas_value()>>self.scale_x) + self.x_offset),
+                        sample_y.eq((point_stream.payload[1].sas_value()>>self.scale_y) + self.y_offset),
+                        sample_p.eq(point_stream.payload[2].sas_value()),
+                        sample_c.eq(point_stream.payload[3].sas_value()),
                         sample_intensity.eq(self.intensity),
                     ]
                     m.next = 'LATCH1'
