0cf8db5f1aa5b7e2692b36a81e181ee166ff8897
Sebastian Holzapfel <me@sebholzapfel.com>
2024-09-07 16:20:16 +0200

    port bootloader bitstream and dependencies

--- gateware/src/boot/top.py
+++ gateware/src/boot/top.py
@@ -6,14 +6,12 @@ import logging
 import os
 
 from amaranth                            import *
-from tiliqua.tiliqua_soc                 import TiliquaSoc
+from tiliqua.tiliqua_soc                 import TiliquaSoc, top_level_cli
 from tiliqua.tiliqua_platform            import set_environment_variables
-from luna_soc                            import top_level_cli
 
 if __name__ == "__main__":
     dvi_timings = set_environment_variables()
     this_directory = os.path.dirname(os.path.realpath(__file__))
     design = TiliquaSoc(firmware_path=os.path.join(this_directory, "fw/firmware.bin"),
                         dvi_timings=dvi_timings)
-    design.genrust_constants()
     top_level_cli(design)
