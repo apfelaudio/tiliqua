1144a10dd18bd548011cda2b98b5d5ca0ade0ad4
Sebastian Holzapfel <me@sebholzapfel.com>
2024-10-02 14:00:07 +0200

    test: fix delayln test cases

--- gateware/tests/test_delayln.py
+++ gateware/tests/test_delayln.py
@@ -10,7 +10,7 @@ from amaranth              import *
 from amaranth.sim          import *
 from amaranth.lib          import wiring
 from amaranth.lib.wiring   import In, Out
-from tiliqua               import dsp, eurorack_pmod, cache
+from tiliqua               import dsp, eurorack_pmod, cache, delay_line
 from tiliqua.eurorack_pmod import ASQ
 
 from amaranth_soc          import csr
@@ -18,76 +18,28 @@ from amaranth_soc          import wishbone
 
 from amaranth_future       import fixed
 
-class WishboneAdapter(wiring.Component):
-    def __init__(self, addr_width_i, addr_width_o, base):
-        self.base = base
-        super().__init__({
-            "i": In(wishbone.Signature(addr_width=addr_width_i,
-                                       data_width=16,
-                                       granularity=8)),
-            "o": Out(wishbone.Signature(addr_width=addr_width_o,
-                                        data_width=32,
-                                        granularity=8,
-                                        features={'bte', 'cti'})),
-        })
-
-    def elaborate(self, platform):
-        m = Module()
-
-        m.d.comb += [
-            self.i.ack.eq(self.o.ack),
-            self.o.adr.eq((self.base<<2) + (self.i.adr>>1)),
-            self.o.we.eq(self.i.we),
-            self.o.cyc.eq(self.i.cyc),
-            self.o.stb.eq(self.i.stb),
-        ]
-
-        with m.If(self.i.adr[0]):
-            m.d.comb += [
-                self.i.dat_r.eq(self.o.dat_r>>16),
-                self.o.sel  .eq(self.i.sel<<2),
-                self.o.dat_w.eq(self.i.dat_w<<16),
-            ]
-        with m.Else():
-            m.d.comb += [
-                self.i.dat_r.eq(self.o.dat_r),
-                self.o.sel  .eq(self.i.sel),
-                self.o.dat_w.eq(self.i.dat_w),
-            ]
-
-        return m
-
 class DelayLineTests(unittest.TestCase):
 
-    def test_persist(self):
+    def test_psram_delayln(self):
 
-        m = Module()
-
-        l2c = cache.WishboneL2Cache(cachesize_words=4)
-
-        dut = dsp.DelayLineWriter(
-            max_delay=16
+        dut = delay_line.DelayLine(
+            max_delay=256,
+            psram_backed=True,
+            base=0x0,
+            addr_width_o=22,
+            write_triggers_read=False,
         )
 
         tap1 = dut.add_tap()
         tap2 = dut.add_tap()
 
-        adapter = WishboneAdapter(addr_width_i=dut.bus.addr_width,
-                                  addr_width_o=l2c.master.addr_width,
-                                  base=0x0)
-
-        wiring.connect(m, dut.bus, adapter.i)
-        wiring.connect(m, adapter.o, l2c.master)
-
-        m.submodules += [l2c, dut, adapter]
-
         async def stimulus_wr(ctx):
             for n in range(0, sys.maxsize):
-                ctx.set(dut.sw.valid, 1)
-                ctx.set(dut.sw.payload,
+                ctx.set(dut.i.valid, 1)
+                ctx.set(dut.i.payload,
                         fixed.Const(0.8*math.sin(n*0.2), shape=ASQ))
                 await ctx.tick()
-                ctx.set(dut.sw.valid, 0)
+                ctx.set(dut.i.valid, 0)
                 await ctx.tick().repeat(30)
 
         async def stimulus_rd1(ctx):
@@ -109,9 +61,9 @@ class DelayLineTests(unittest.TestCase):
                 await ctx.tick().repeat(30)
 
         async def testbench(ctx):
-            # Simulate some acks
-            mem = [0] * 16
-            membus = l2c.slave
+            # Simulate some transactions against a fake PSRAM bus.
+            mem = [0] * dut.max_delay
+            membus = dut.bus
             for _ in range(200):
                 while not ctx.get(membus.stb):
                     await ctx.tick()
@@ -120,14 +72,8 @@ class DelayLineTests(unittest.TestCase):
                 ctx.set(membus.ack, 1)
                 adr = ctx.get(membus.adr)
                 if ctx.get(membus.we):
-                    if ctx.get(membus.sel == 0b0011):
-                        mem[adr] = mem[adr] & 0xFFFF0000
-                        mem[adr] |= ctx.get(membus.dat_w & 0xFFFF)
-                    elif ctx.get(membus.sel == 0b1100):
-                        mem[adr] = mem[adr] & 0x0000FFFF
-                        mem[adr] |= ctx.get(membus.dat_w & 0xFFFF0000)
-                    else:
-                        mem[adr] = ctx.get(membus.dat_w)
+                    # warn: only whole-word transactions are simulated
+                    mem[adr] = ctx.get(membus.dat_w)
                     print("write", hex(mem[adr]), "@", adr)
                 else:
                     print("read", hex(mem[adr]), "@", adr)
@@ -136,7 +82,7 @@ class DelayLineTests(unittest.TestCase):
                 ctx.set(membus.ack, 0)
                 await ctx.tick()
 
-        sim = Simulator(m)
+        sim = Simulator(dut)
         sim.add_clock(1e-6)
         sim.add_testbench(testbench)
         sim.add_process(stimulus_wr)
