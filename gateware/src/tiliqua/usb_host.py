d60bec16ab690c63eea3eec110b1e76a0f2d9669
Sebastian Holzapfel <me@sebholzapfel.com>
2024-10-31 20:14:53 +0100

    SOF makes it out Tiliqua :)

--- gateware/src/tiliqua/usb_host.py
+++ gateware/src/tiliqua/usb_host.py
@@ -12,28 +12,31 @@ class USBSOFPacketGenerator(Elaboratable):
     _PACKET_SOF   = 0b10100101
 
     def __init__(self, standalone=False):
+        self.en_sof = Signal()
         self.tx = UTMITransmitInterface()
 
     def elaborate(self, platform):
         m = Module()
 
         frame_number = Signal(11, reset=0)
-        sof_timer = Signal(32)
-        send_sof = Signal()
-
-        # TODO: s/600/60000 !!!!!!!!!!!!!!!
-        with m.If(sof_timer == (600 - 1)):
-            m.d.usb += sof_timer.eq(0)
-            m.d.usb += frame_number.eq(frame_number + 1)
-            m.d.comb += send_sof.eq(1)
-        with m.Else():
-            m.d.usb += sof_timer.eq(sof_timer + 1)
 
         with m.FSM(domain="usb"):
 
+            with m.State('SOF-OFF'):
+                with m.If(self.en_sof):
+                    m.next = 'IDLE'
+
             with m.State('IDLE'):
-                with m.If(send_sof):
+                sof_timer = Signal(16)
+                with m.If(sof_timer == (60000 - 1)):
+                    m.d.usb += sof_timer.eq(0)
+                    m.d.usb += frame_number.eq(frame_number + 1)
                     m.next = "SEND_PID"
+                with m.Else():
+                    m.d.usb += sof_timer.eq(sof_timer + 1)
+
+                with m.If(~self.en_sof):
+                    m.next = 'SOF-OFF'
 
             with m.State('SEND_PID'):
                 m.d.comb += [
@@ -72,10 +75,7 @@ class SimpleUSBHost(Elaboratable):
             self.utmi = UTMIInterface()
         else:
             self.utmi = UTMITranslator(ulpi=bus, handle_clocking=handle_clocking)
-            self.bus_busy   = self.utmi.busy
             self.translator = self.utmi
-        self.always_fs  = False
-        self.data_clock = 60e6
 
     def elaborate(self, platform):
 
@@ -103,14 +103,30 @@ class SimpleUSBHost(Elaboratable):
             data_crc.tx_data       .eq(tx_multiplexer.output.data),
         ]
 
-        # UTMI host mode settings
         m.d.comb += [
             self.utmi.dm_pulldown.eq(1), # enable host pulldowns
             self.utmi.dp_pulldown.eq(1),
-            self.utmi.op_mode.eq(0), # normal
-            self.utmi.xcvr_select.eq(0x01), # FS
-            self.utmi.term_select.eq(1),
         ]
+
+
+        cnt = Signal(64)
+        m.d.sync += cnt.eq(cnt+1)
+        # 100ms bus reset
+        bus_reset = cnt < 6000000
+        with m.If(bus_reset):
+            m.d.comb += [
+                sof_generator.en_sof.eq(0),
+                self.utmi.op_mode.eq(UTMIOperatingMode.RAW_DRIVE),
+                self.utmi.xcvr_select.eq(USBSpeed.HIGH),
+                self.utmi.term_select.eq(UTMITerminationSelect.HS_NORMAL),
+            ]
+        with m.Else():
+            m.d.comb += [
+                sof_generator.en_sof.eq(1),
+                self.utmi.op_mode.eq(UTMIOperatingMode.NORMAL),
+                self.utmi.xcvr_select.eq(USBSpeed.FULL),
+                self.utmi.term_select.eq(UTMITerminationSelect.LS_FS_NORMAL),
+            ]
 
         return m
 
